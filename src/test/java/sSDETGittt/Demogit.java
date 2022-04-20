63780a702a6b4d80e3000ccc65b08884626c9981
varunkmkumar <varunhvh97@gmail.com>
2022-04-20 18:20:14 +0530

    Again commit

--- src/test/java/sSDETGittt/Demogit.java
+++ src/test/java/sSDETGittt/Demogit.java
@@ -8,6 +8,8 @@ public class Demogit {
 	{
 		System.out.println("FIRST COMMIT");
 		System.out.println("second cOMMIT");
+		System.out.println("FIRST COMMIT");
+		System.out.println("second cOMMIT");
 	}
 
 }
